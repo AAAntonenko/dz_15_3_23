2090487ae92ec26a0c97783fbfc4e40fe4db93e5
Artem Antonenko <a-.-@MacBook-Pro-Artem.local>
2023-03-12 09:57:50 +0300

    first commit

---
+++ задача/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Clear();
+string[] array = new string[] { "Sunday", "Mon", "Tuesday", "Wed", "Thu", "Friday", "Sat" };
+string[] newArray = new string[array.Length];
+
+void CreateArray (string[] inArray, string[] outArray)
+{   
+    int index = 0;
+    for (int i = 0; i < inArray.Length; i++)
+    {   if(inArray[i].Length <= 3) {outArray[index]=inArray[i]; index++;}}
+}
+CreateArray(array, newArray);
+   
+
+void PrintArray(string[] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        Console.Write($"{inArray[i]} ");
+    }
+        Console.WriteLine();
+}
+
+PrintArray(newArray);
